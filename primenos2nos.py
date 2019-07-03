5ca4f94954d34fb73b87c4555260580b869682c4
SANTHOSHINIPERIYSAMY <50875451+SANTHOSHINIPERIYSAMY@users.noreply.github.com>
2019-07-03 16:44:03 +0530

    Create primenos2nos.py

---
+++ primenos2nos.py
@@ -0,0 +1,13 @@
+c,k=map(int,input("").split())
+i=c+1
+
+while(i<k):
+    sam=0
+    for j in range(2,i):
+        if(i%j==0):
+            sam=1
+            break
+        j+=1
+    if(sam==0):
+        print(i,end=" ")
+    i+=1
